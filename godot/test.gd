bb280e04ffdd681d310d0eb53dbaaf23d7ca7747
gtalpo <gtalpo@gmail.com>
2025-04-13 18:02:51 +0900

    apply audio generator demo

--- godot/test.gd
+++ godot/test.gd
@@ -9,3 +9,31 @@ func _on_button_pressed() -> void:
 func _on_player_speed_increased() -> void:
 	print("speed_increased received")
 	pass # Replace with function body.
+
+
+var playback # Will hold the AudioStreamGeneratorPlayback.
+@onready var sample_hz = $AudioStreamPlayer.stream.mix_rate
+var pulse_hz = 440.0 # The frequency of the sound wave.
+var phase = 0.0
+
+var rng = RandomNumberGenerator.new()
+
+func _ready():
+	$AudioStreamPlayer.play()
+	playback = $AudioStreamPlayer.get_stream_playback()
+	fill_buffer()
+
+func fill_buffer():
+	var increment = pulse_hz / sample_hz
+	var frames_available = playback.get_frames_available()
+
+	for i in range(frames_available):
+		playback.push_frame(Vector2.ONE * sin(phase * TAU))
+		phase = fmod(phase + increment, 1.0)
+
+func _process(delta: float) -> void:
+	fill_buffer();
+	
+func _on_button_play_pressed() -> void:
+	pulse_hz = rng.randf_range(130.8, 987.8)
+	print("pulse changed %s" % pulse_hz)
